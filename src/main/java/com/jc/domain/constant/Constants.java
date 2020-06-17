3bfffb9631b2134eea460d7163ef0465baeba289
zhaojingchun <zhaojingchun@jd.com>
2020-06-17 21:51:22 +0800

    add

--- src/main/java/com/jc/domain/constant/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.jc.domain.constant;
-
-/**
- * Created by zhaojingchun on 2017/9/14.
- */
-public class Constants {
-    //JSONP参数名
-    public static final String PARAM_JSONP = "callback";
-}
